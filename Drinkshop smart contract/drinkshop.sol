de5eb7bcf7ed7c9b0f4e1e7b8f617e04288dce4f
ishwarsinghnaruka <49265644+ishwarsinghnaruka@users.noreply.github.com>
2022-03-31 17:45:27 +0530

    Create drinkshop.sol

---
+++ Drinkshop smart contract/drinkshop.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+contract DrinkShop {
+    address public drinkshopowner;
+    mapping (address => uint256) public drinkLiterBalance;
+
+    constructor() {
+        drinkshopowner = msg.sender;
+        drinkLiterBalance[address(this)] = 100;
+    }
+
+    function getDrinkShopBalance() public view returns (uint256) {
+        return drinkLiterBalance[address(this)];
+    }
+
+    function AddDrinks(uint256 liter) public {
+        require(msg.sender == drinkshopowner,"Shop owner can add drink to the machine");
+        drinkLiterBalance[address(this)] += liter;
+    }
+
+    function purchase(uint liter) public payable {
+        require(msg.value >= liter * 1 ether, "Price is 0.2 ether per donut");
+        require(drinkLiterBalance[address(this)] >= liter, "drinks not available");
+        drinkLiterBalance[address(this)] -= liter;
+        drinkLiterBalance[msg.sender] += liter;
+    }
+}
